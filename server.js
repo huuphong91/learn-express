95ccf54d198ec7243c2777ea42780147b55a3bde
huuphong91 <huuphong91@gmail.com>
2020-07-28 16:29:03 +0700

    - finish section 8

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const DB = process.env.DATABASE_LOCAL;
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(con => {
-  console.log('DB connection successful');
-});
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
